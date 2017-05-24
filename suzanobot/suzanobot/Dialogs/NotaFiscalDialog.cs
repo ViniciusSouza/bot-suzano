e753508813a8ba56cba9b5907a0eade30f2cf199
ViniciusSouza <visouza@microsoft.com>
2017-05-24 10:26:19 -0300

    checking the user response adding Documento model  (Azure Search item)

--- suzanobot/suzanobot/Dialogs/NotaFiscalDialog.cs
+++ suzanobot/suzanobot/Dialogs/NotaFiscalDialog.cs
@@ -14,11 +14,27 @@ namespace suzanobot.Dialogs
         {
             string message = "Qual o status da sua nota fical?";
 
-            PromptDialog.Choice(context, ResumeAfterAnswer, new[] { "Não Lançada", "Lançada", "Em Proocessamento", "Não Sei" }, message);
+            PromptDialog.Choice(context, ResumeAfterAnswer, new[] { "Não Lançada", "Lançada", "Em Processamento", "Não Sei" }, message);
         }
 
         private async Task ResumeAfterAnswer(IDialogContext context, IAwaitable<string> result)
         {
+            var message = context.MakeMessage();
+
+            switch (await result)
+            {
+                case "Não Lançada":
+                    message.Text = string.Format("## Opção de nota fical **Não Lançada** \n\n Acesse o link [Catalago Fiscal](https://suzanoprod.service-now.com/csc/csc_catalogo_fiscal.do)");
+                    break;
+                case "Lançada":
+                    break;
+                case "Em Processamento":
+                    break;
+                case "Não Sei":
+                    break;
+            }
+            await context.PostAsync(message);
+
             context.Done(await result);
         }
     }
